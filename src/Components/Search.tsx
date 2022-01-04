0e9e01f48acad721d3c6c2c93d05ebccfdbb06d4
Katherine Nguyen <nguyenkat01@yahoo.com>
2022-01-03 18:40:20 -0800

    fixed object object on search bar

--- src/Components/Search.tsx
+++ src/Components/Search.tsx
@@ -1,38 +1,38 @@
 import React, { useState, useEffect } from "react";
-
+import SearchResult from "../Pages/SearchResult";
 
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const Search: React.FC = () => {
   const [keyword, setKeyword] = useState<string>("");
+  const [results, setResults] = useState<[]>([]);
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
   useEffect(() => {
-    fetchData();
+    fetchData(keyword);
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = async (keyword): Promise<any> => {
     await fetch(
       `https://newsapi.org/v2/everything?q=${keyword}&language=en&sortBy=relevancy&apiKey=${API_KEY}`
-    ).then((response) => {
-      response
-        .json()
-        .then((res) => {
-          setKeyword(res);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    });
+    )
+      .then((response) => response.json())
+      .then((res) => setResults(res))
+      .catch((error) => {
+        console.log("Error:", error);
+      });
   };
 
+  console.log(results)
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    console.log(event.target.value);
     setKeyword(event.target.value);
   };
 
   const handleSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
-    await fetchData();
+    await fetchData(keyword);
     setIsLoaded(true);
     setKeyword("");
   };
@@ -48,7 +48,9 @@ const Search: React.FC = () => {
           onChange={handleChange}
           onClick={handleSubmit}
         ></input>
+        <button onClick={handleSubmit}>Search</button>
       </form>
+      {!isLoaded && <SearchResult results={results} articles={[]} title={""} author={""} publishedAt={""} description={""}/>}
     </div>
   );
 };
